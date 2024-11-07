3695cb801e1c63656fadbffa27080836dc230a52
tienv1per <tienv1perds@gmail.com>
2024-11-07 11:51:35 +0700

    add leetcode 28 cpp

---
+++ easy/108-indexFirstOccurenceString.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size();
+        int n = needle.size();
+        for(int i = 0; i < m + 1 - n; i++){
+            for(int j = 0; j < n; j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+                if(j == n - 1) return i;
+            }    
+        }
+
+        return -1;
+    }
+};
